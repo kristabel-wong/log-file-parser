fbe15d18fcabd814215a519d2757546e9c8f1890
Kristabel Wong <kristabel.wong@hotmail.com>
2022-08-17 15:46:21 +1000

    DRY-ing up test code and upated readme

--- spec/main.test.js
+++ spec/main.test.js
@@ -1,10 +1,18 @@
 const logAnalysis = require('../logAnalysis');
 
+const expectedTopIPs = [
+    [ '168.41.191.40', 4 ],
+    [ '177.71.128.21', 3 ],
+    [ '50.112.00.11', 3 ]
+];
+
+const expectedTopURLs = [
+    [ '/faq/', 2 ],
+    [ '/docs/manage-websites/', 2 ],
+    [ '/intranet-analytics/', 1 ]
+];
+
 describe('logAnalysis', () => {
-    // beforeEach(() => {
-    //     // read the file before each test example
-    //     logAnalysis.loadFile('programming-task-example-data_(1).log');
-    // }); 
 
     test('readTheFile should read the file contents', () => {
         logAnalysis.loadFile('programming-task-example-data_(1).log');
@@ -19,12 +27,7 @@ describe('logAnalysis', () => {
         test('getTopIPs should return a sorted array of entries of requested size', () => {
             const top3 = logAnalysis.getTopIPs(3);
             expect(top3.length).toBe(3);
-            expect(top3[0][0]).toBe('168.41.191.40');
-            expect(top3[0][1]).toBe(4);
-            expect(top3[1][0]).toBe('177.71.128.21');
-            expect(top3[1][1]).toBe(3);
-            expect(top3[2][0]).toBe('50.112.00.11');
-            expect(top3[2][1]).toBe(3);
+            expect(top3).toEqual(expectedTopIPs);
         });
 
     }); // end of IP related stats testing
@@ -33,12 +36,7 @@ describe('logAnalysis', () => {
         test('getMostVisitedURLs should return a sorted array of entries of requested size', () => {
             const top3 = logAnalysis.getMostVisitedURLs(3);
             expect(top3.length).toBe(3);
-            expect(top3[0][0]).toBe('/faq/');
-            expect(top3[0][1]).toBe(2); // 8
-            expect(top3[1][0]).toBe('/docs/manage-websites/');
-            expect(top3[1][1]).toBe(2); // 8
-            expect(top3[2][0]).toBe('/intranet-analytics/');
-            expect(top3[2][1]).toBe(1);
+            expect(top3).toEqual(expectedTopURLs);
         });
 
     }); // end of URL related stats testing
